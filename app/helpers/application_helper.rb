54a8b0f18834a40bbbbe31c74e6d964eebc88d36
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2020-12-01 08:06:34 +0000

    static_page_layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    
+ def title_name(page_name = "")
+   base_title = "Attendance App"
+   if page_name.nil?
+       base_title
+   else
+     page_name + "|" + base_title
+   end
+ end
 end
