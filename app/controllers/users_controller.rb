c42a70efe11fb7f2fddaa61f8a80274b3b9abc5e
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2020-12-01 15:17:58 +0000

    新規作成機能　終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,25 @@
 class UsersController < ApplicationController
+  
+  def show
+    @user = User.find(params[:id])
+  end
   def new
     @user = User.new
   end
+  
+  def create
+    @user = User.new(params_user)
+    if @user.save
+      flash[:success] = "ユーザーを新規作成しました。"
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+  
+  
+  private
+    def params_user
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
