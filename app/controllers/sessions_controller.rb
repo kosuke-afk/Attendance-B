322cfc9e08bff7338125e3f899757107ef61911e
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2020-12-02 15:49:12 +0000

    一時的ログイン機能　終了

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def new
+  end
+  
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in user
+      flash[:success] = "ログインしました。"
+      redirect_to user
+    else
+      flash[:danger] = "ログインに失敗しました。"
+      render :new
+    end
+  end
+
+  
+end
