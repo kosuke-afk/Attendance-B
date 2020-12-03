1a5e074d1b4b81f40969210beb0856914f8d6b94
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2020-12-03 08:58:46 +0000

    login-function-conplete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,22 @@ class SessionsController < ApplicationController
   end
   
   def create
-    user = User.find_by(email: params[:session][:email])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == 1 ? remember(user) : forget(user)
       flash[:success] = "ログインしました。"
       redirect_to user
     else
-      flash[:danger] = "ログインに失敗しました。"
+      flash.now[:danger] = "ログインに失敗しました。"
       render :new
     end
   end
-
+  
+  def destroy
+    log_out if logged_in?
+    flash[:success] = "ログアウトしました。"
+    redirect_to root_url
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -10,4 +11,37 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   
   has_secure_password
+  
+  # 渡された文字列のハッシュ化
+  def User.digest(string)
+    cost = 
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
+  # ランダムな文字列を返す。
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+  
+  # 永続化セッションのためにremember_digestを保存する。
+  def remember
+    self.remember_token = User.new_token # 認証に必要な仮属性のremember_tokenにハッシュ化する前の値を入れている。
+    update_attribute(:remember_digest, User.digest(remember_token))  # remember_digestにremember_tokenをハッシュ化したものを入れて更新
+  end
+  
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
+  # remember_tokenとremember_digestの認証に使う。 トークンとダイジェストが一致すれば、true
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+  
+  
 end
