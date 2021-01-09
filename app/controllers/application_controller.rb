fd43b6356e61ef8734afee15e08b4527ba8a8292
EC2 Default User <ec2-user@ip-172-31-16-191.ap-northeast-1.compute.internal>
2021-01-09 08:48:30 +0000

    challenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,9 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
   
-  $days_of_the_week = %w{日 月 火 水 木 金 土}
+  $days_of_the_week = %w{日 月 火 水 木 金 土} # リテラル表記による配列
   
+  # userオブジェクトの取得と代入
   def set_user
     @user = User.find(params[:id])
   end
@@ -35,9 +36,9 @@ class ApplicationController < ActionController::Base
    end
   
   
-  
+  #  ユーザーの勤怠情報の取得・無ければ作成
   def set_one_month
-    @first_day = params[:date].nil? ?
+    @first_day = params[:date].nil? ? 
     Date.current.beginning_of_month : params[:date].to_date
     @last_day = @first_day.end_of_month
     one_month = [*@first_day..@last_day]
